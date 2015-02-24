b692b0adc1c6d917bb1c59d641198a66ba6a0ff6
Alison Medlyn <medlyna@gmail.com>
2015-02-24 09:34:25 -0500

    added search view after user login

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,4 @@
+class SearchController < ApplicationController
+	def search_recipes
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   resources :recipes
 
   resources :users
+  root "search#search_recipes"
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
